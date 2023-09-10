3dd2b37b935ef59d6781984e6f8e7dff1f5902f7
Manami <vaattii1996@gmail.com>
2023-09-10 16:21:22 -0700

    delete user avator

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -60,13 +60,6 @@ const NavBar = () => {
       <Toolbar className={classes.toolbar}>
         {user ? (
           <div className={classes.profile}>
-            <Avatar
-              className={classes.purple}
-              alt={user.result.name}
-              src={user.result.imageUrl}
-            >
-              {user.result.name.charAt[0]}
-            </Avatar>
             <Typography className={classes.userName} variant="h6">
               {user.result.name}
             </Typography>
